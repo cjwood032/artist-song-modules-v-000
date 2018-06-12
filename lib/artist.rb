b26c00804ddba0d601213b05c7e484bff2c31718
CHRISTOPHER WOOD <cjwood032@gmail.com>
2018-06-12 22:59:28 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,7 +1,7 @@
 require 'pry'
 require_relative "../lib/concerns/memorable.rb"
-extend Memorable::ClassMethods
 class Artist
+  extend Memorable::ClassMethods
   attr_accessor :name
   attr_reader :songs
 
