38d51f172e40f91c0cf6ae56c6e881116b86cefb
CHRISTOPHER WOOD <cjwood032@gmail.com>
2018-06-12 22:56:24 +0000

    automatically backed up by learn

--- lib/concerns/memorable.rb
+++ lib/concerns/memorable.rb
@@ -1,8 +1,9 @@
 module Memorable
-  def self.count
-    self.all.count
-  end
-  def self.reset_all
-    self.all.clear
-  end
+  module ClassMethods
+    def reset_all
+      self.all.clear
+    end
+    def count
+      self.all.count
+    end
 end
